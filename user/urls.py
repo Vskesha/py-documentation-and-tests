62ed9db1f69c449ee20c2a353958204d39c2e99f
Vasyl Boliukh <vskesha@gmail.com>
2025-09-11 00:24:13 +0300

    removed TokenAuthentication, replaced with JWT tokens

--- user/urls.py
+++ user/urls.py
@@ -4,13 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView
 )
 
-from user.views import CreateUserView, CreateTokenView, ManageUserView
+from user.views import CreateUserView, ManageUserView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
     path("api/token/", TokenObtainPairView.as_view(), name="token"),
     path(
